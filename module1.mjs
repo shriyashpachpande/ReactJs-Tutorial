106699375725d23c57e937b6b8bc6b620555e43c
shriyashpachpande <shriyashpachpande813@gmail.com>
2025-04-02 06:24:16 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import dza, {c,d,a} from './module2.mjs'
+
+console.log(dza)
+console.log(c)
+console.log(d)
+console.log(a)
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "shriyash"
+let b = "Rohan"
+let c = "Priyanka"
+let d= "Aakash"
+
+export default b;
+export {a}
+export{b}
+export{c}
